fab854e7627c1b25703011ef0dc6725e2311421e
malcolm staso <mstaso1@gmail.com>
2020-04-21 16:22:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  numbers = [1, 2, 3, 4]
+  array = [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array
@@ -11,7 +11,7 @@ def add_element_to_end_of_array
 end
 
 
-def add_element_to_start_of_array(array, element)
+def add_element_to_start_of_array(numbers, 5)
   
 end
 
