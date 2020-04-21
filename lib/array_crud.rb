6906a897dba571d5fb337e6aecd1f777670a4903
malcolm staso <mstaso1@gmail.com>
2020-04-21 16:26:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,7 @@ def create_an_array
   colors = ["red", "blue", "green", "yellow"]
 end
 
-def add_element_to_end_of_array(colors, white)
+def add_element_to_end_of_array(colors, white).last
   white = "white"
 colors.push(white)
 end
