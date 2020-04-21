0df60448bfa4a832aa037ea5968da099e46dc3a0
malcolm staso <mstaso1@gmail.com>
2020-04-21 16:30:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ def add_element_to_start_of_array(colors, black)
 end
 
 def remove_element_from_end_of_array(colors)
-  c
+  white = colors.pop
 end
 
 def remove_element_from_start_of_array(array)
